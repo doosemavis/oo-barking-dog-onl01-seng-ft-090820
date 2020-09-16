bac5055cc94411af0be1a719e1278c988470ad18
Moose Davis <moosedavis2011@gmail.com>
2020-09-16 21:33:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
+class Dog
+  def name= (dogs_name)
+    @this_dogs_name = dogs_name
+  end 
+  
+  def bark= ("Woof!")
+    @dogs_bark = "Woof!"
+end 
 
+
+fido = Dog.new 
+fido.name = "Fido"
+
+fido.name
+
+fido.bark
